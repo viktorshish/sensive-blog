a727c24c6cfd10814d547dbb8ff24176eedd532b
Shishkin PRO <viktorshish@gmail.com>
2024-04-29 22:37:53 +0300

    Show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -28,8 +28,9 @@ def serialize_tag(tag):
 
 
 def index(request):
-
-    most_popular_posts = []  # TODO. Как это посчитать?
+    posts = Post.objects.all()
+    popular_posts = sorted(posts, key=get_likes_count)
+    most_popular_posts = popular_posts[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -46,6 +47,10 @@ def index(request):
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
+
+
+def get_likes_count(post):
+    return post.likes.count()
 
 
 def post_detail(request, slug):
